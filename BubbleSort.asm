92c3795bccc6c5744ee0a89bbda341ff6700ee73
RithikaSapparapu <70932495+RithikaSapparapu@users.noreply.github.com>
2021-03-08 22:03:53 +0530

    final BubbleSort

--- BubbleSort.asm
+++ BubbleSort.asm
@@ -1,17 +1,11 @@
 .data
-    .word 9 # 0x10010000 - 0001 0000 0000 0001 0000 0000 0000 0000 
-    .word 3
-    .word 1
-    .word 5
-    
-    # array: .word 9,3,1,5
+    array: .word 9, 3, 1, 5, 6, 10, 2
 .text
 .globl main
 main:
     addi $t6, $zero, 1
     addi $t5, $zero, 1
     j bigwhile
-
 while:
     lw $t0, 0($s0)
     lw $t1, 4($s0)
@@ -21,24 +15,18 @@ while:
     sw $t0, 4($s0)
     add $t5, $t5, $t6
     addi $s0, $s0, 4
-
     lw $t3, 4($s0)
     beq $t3, $zero, bigwhile
-
     j while
-
 increment: 
     addi $s0, $s0, 4
     lw $t3, 4($s0)
     beq $t3, $zero, bigwhile
     j while
-
 bigwhile:
-    lui $s0, 0x1001
-
+    la $s0, array
     beq $t5, $zero, exit
     addi $t5, $zero, 0
     j while
-
 exit:
     jr $ra
